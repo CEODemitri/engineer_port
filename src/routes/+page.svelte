194c0c7b5220b38e66a27ffed731d4b548815b98
ceodemitri <demichole@gmail.com>
2025-10-14 17:27:14 -0500

    refactor:  site structure add for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,27 @@
 			'Welcome to my portfolio website where I showcase my projects, skills, and experiences as an engineer.',
 		keywords: 'engineer, portfolio, projects, skills, experience, coding, development'
 	};
+
+	const meOneMeta = {
+		sectionTitle: 'Career Objectives',
+		subtitle: 'Journey Ahead',
+		description: 'To secure a mid-level software engineering position where I will apply my technical expertise, analytical mindset, and passion for development to build impactful software solutions. I am actively seeking oppurtunities that align with my strengths in web and software development. I am open to roles that challenge me to grow further in cloud technologies and system design.',
+		keywords: ''
+	}
+
+	const meTwoMeta = {
+		sectionTitle: 'Core Skills',
+		subtitle: 'Focused and Intentional Growth',
+		description: '',
+		keywords: ''
+	}
+
+	const valueMeta = {
+		sectionTitle: 'Value',
+		subtitle: 'What I bring to the Table',
+		description: '',
+		keywords: ''
+	}
 </script>
 
 <!-- hero section-->
@@ -21,28 +42,34 @@
 		<a href="#" class="bg-black text-white p-1 px-4 b">Projects</a>
 		<a href="#">Resume</a>
 	</div>
+
+	<hr>
 </section>
 
 <!-- Test -->
 <!--<SectionTitle {...homeMeta} /> -->
+
+<!-- about me section -->
 <div>
-	<section id="about">
+	<section id="about" class="h-[60vh]">
 		<h2>About Me</h2>
-		<p>
-			I am a passionate engineer with experience in software development, problem-solving, and
-			innovative solutions. I enjoy working on challenging projects and continuously learning new
-			technologies.
-		</p>
+		<div>
+			<SectionTitle {...meOneMeta} />
+		</div>
+
+		<div>
+			<SectionTitle {...meTwoMeta}/>
+			<ul>
+				<li>Programming: Python, C++, JavaScript</li>
+				<li>Web: HTML, REST APIs</li>
+				<li>Tools: Git, Docker, Linux</li>
+				<li>Other: Data Structures, Algorithms, Debugging</li>
+			</ul>
+		</div>
 	</section>
 
-	<section id="skills">
-		<h2>Skills</h2>
-		<ul>
-			<li>Programming: Python, C++, JavaScript</li>
-			<li>Web: HTML, REST APIs</li>
-			<li>Tools: Git, Docker, Linux</li>
-			<li>Other: Data Structures, Algorithms, Debugging</li>
-		</ul>
+	<section id="benefit">
+		<SectionTitle {...valueMeta}/>
 	</section>
 </div>
 
